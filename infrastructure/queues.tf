047d4f22ff30ef1886e63cde683056b520901191
Rio Knightley <rio.knightley2@nhs.net>
2023-09-13 14:59:43 +0100

    Add nems sns subscription

--- infrastructure/queues.tf
+++ infrastructure/queues.tf
@@ -1,36 +1,36 @@
 module "sqs-splunk-queue" {
   source = "./modules/sqs"
-  name   = "sqs-splunk-queue"
+  name   = "splunk-queue"
 }
 
 module "sqs-nems-queue" {
   source = "./modules/sqs"
-  name   = "sqs-nems-queue"
+  name   = "nems-queue"
 }
 
-# module "sqs-nems-queue-subscription" {
-#   source         = "./modules/sns"
-#   topic_name     = "create_doc-alarms-topic"
-#   topic_protocol = "lambda"
-#   topic_endpoint = module.create-doc-ref-lambda.endpoint
-#   delivery_policy = jsonencode({
-#     "Version" : "2012-10-17",
-#     "Statement" : [
-#       {
-#         "Effect" : "Allow",
-#         "Principal" : {
-#           "Service" : "cloudwatch.amazonaws.com"
-#         },
-#         "Action" : [
-#           "SNS:Publish",
-#         ],
-#         "Condition" : {
-#           "ArnLike" : {
-#             "aws:SourceArn" : "arn:aws:cloudwatch:eu-west-2:${data.aws_caller_identity.current.account_id}:alarm:*"
-#           }
-#         }
-#         "Resource" : "*"
-#       }
-#     ]
-#   })
-# }
+module "sqs-nems-queue-subscription" {
+  source         = "./modules/sns"
+  topic_name     = "create_doc-alarms-topic"
+  topic_protocol = "sqs"
+  topic_endpoint = module.sqs-nems-queue.endpoint
+  delivery_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudwatch.amazonaws.com"
+        },
+        "Action" : [
+          "SNS:Publish",
+        ],
+        "Condition" : {
+          "ArnLike" : {
+            "aws:SourceArn" : "arn:aws:cloudwatch:eu-west-2:${data.aws_caller_identity.current.account_id}:alarm:*"
+          }
+        }
+        "Resource" : "*"
+      }
+    ]
+  })
+}
