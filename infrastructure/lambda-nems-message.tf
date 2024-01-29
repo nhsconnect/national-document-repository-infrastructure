635e190c0c687e003ffb4294ada8db7cfb11e7ca
Rio Knightley <rio.knightley2@nhs.net>
2024-01-29 09:54:54 +0000

    Add depends on for mesh forwader

--- infrastructure/lambda-nems-message.tf
+++ infrastructure/lambda-nems-message.tf
@@ -19,50 +19,9 @@ module "nems-message-lambda" {
   is_invoked_from_gateway       = false
 
   depends_on = [
+    aws_iam_role.mesh_forwarder,
     aws_api_gateway_rest_api.ndr_doc_store_api,
     module.lloyd_george_reference_dynamodb_table,
     module.sqs-nems-queue,
   ]
 }
-
-# module "bulk-upload-metadata-alarm" {
-#   source               = "./modules/lambda_alarms"
-#   lambda_function_name = module.bulk-upload-metadata-lambda.function_name
-#   lambda_timeout       = module.bulk-upload-metadata-lambda.timeout
-#   lambda_name          = "bulk_upload_metadata_handler"
-#   namespace            = "AWS/Lambda"
-#   alarm_actions        = [module.bulk-upload-metadata-alarm-topic.arn]
-#   ok_actions           = [module.bulk-upload-metadata-alarm-topic.arn]
-#   depends_on           = [module.bulk-upload-metadata-lambda, module.bulk-upload-metadata-alarm-topic]
-# }
-
-# module "bulk-upload-metadata-alarm-topic" {
-#   source                = "./modules/sns"
-#   sns_encryption_key_id = module.sns_encryption_key.id
-#   current_account_id    = data.aws_caller_identity.current.account_id
-#   topic_name            = "bulk-upload-metadata-topic"
-#   topic_protocol        = "lambda"
-#   topic_endpoint        = module.bulk-upload-metadata-lambda.endpoint
-#   delivery_policy = jsonencode({
-#     "Version" : "2012-10-17",
-#     "Statement" : [
-#       {
-#         "Effect" : "Allow",
-#         "Principal" : {
-#           "Service" : "cloudwatch.amazonaws.com"
-#         },
-#         "Action" : [
-#           "SNS:Publish",
-#         ],
-#         "Condition" : {
-#           "ArnLike" : {
-#             "aws:SourceArn" : "arn:aws:cloudwatch:eu-west-2:${data.aws_caller_identity.current.account_id}:alarm:*"
-#           }
-#         }
-#         "Resource" : "*"
-#       }
-#     ]
-#   })
-
-#   depends_on = [module.bulk-upload-metadata-lambda, module.sns_encryption_key]
-# }
