212da945a8039ef648a264503060ca8f3a2ec13b
Joe Fong <joe.fong1@nhs.net>
2024-06-19 14:56:07 +0100

    [PRMP-535] remove ARF bucket name from createDocRef env var (as upload should be via staging bucket only)

--- infrastructure/lambda-create-doc-ref.tf
+++ infrastructure/lambda-create-doc-ref.tf
@@ -86,7 +86,6 @@ module "create-doc-ref-lambda" {
     APPCONFIG_APPLICATION        = module.ndr-app-config.app_config_application_id
     APPCONFIG_ENVIRONMENT        = module.ndr-app-config.app_config_environment_id
     APPCONFIG_CONFIGURATION      = module.ndr-app-config.app_config_configuration_profile_id
-    DOCUMENT_STORE_BUCKET_NAME   = "${terraform.workspace}-${var.docstore_bucket_name}"
     DOCUMENT_STORE_DYNAMODB_NAME = "${terraform.workspace}_${var.docstore_dynamodb_table_name}"
     LLOYD_GEORGE_DYNAMODB_NAME   = "${terraform.workspace}_${var.lloyd_george_dynamodb_table_name}"
     PDS_FHIR_IS_STUBBED          = local.is_sandbox,
