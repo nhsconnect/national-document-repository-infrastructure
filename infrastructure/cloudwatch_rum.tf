5263422b6dfac7a47b611e09ccb8ad4490d67ad3
Ollie Beumkes <oliver.beumkes2@nhs.net>
2025-02-21 13:31:58 +0000

    moved couldwatch rum to root

--- infrastructure/cloudwatch_rum.tf
+++ infrastructure/cloudwatch_rum.tf
@@ -1,4 +1,21 @@
-module "cloudwatch_rum" {
-  source = "./modules/cloudwatch_rum"
+resource "aws_cognito_identity_pool" "rum_identity_pool" {
+  identity_pool_name               = "${terraform.workspace}-rum-identity-pool"
+  allow_unauthenticated_identities = true
+  count = local.is_production ? 0 : 1
+}
+
+resource "aws_cloudwatch_rum_app_monitor" "app_monitor" {
+  name             = "${terraform.workspace}-app-monitor"
+  domain           = "*.patient-deductions.nhs.uk"
+  cw_log_enabled   = true
+  identity_pool_id = aws_cognito_identity_pool.rum_identity_pool.id
+
+  app_monitor_configuration {
+    allow_cookies       = true
+    enable_xray         = true
+    session_sample_rate = 1.0
+    telemetries         = ["errors", "performance", "http"]
+  }
+  
   count = local.is_production ? 0 : 1
 }
