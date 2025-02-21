37b290f5cbd340c9f2aea901e02c9eda558dac75
Ollie Beumkes <oliver.beumkes2@nhs.net>
2025-02-21 13:37:42 +0000

    Fixed formatting

--- infrastructure/cloudwatch_rum.tf
+++ infrastructure/cloudwatch_rum.tf
@@ -1,14 +1,15 @@
 resource "aws_cognito_identity_pool" "rum_identity_pool" {
+  count                            = local.is_production ? 0 : 1
   identity_pool_name               = "${terraform.workspace}-rum-identity-pool"
   allow_unauthenticated_identities = true
-  count = local.is_production ? 0 : 1
 }
 
 resource "aws_cloudwatch_rum_app_monitor" "app_monitor" {
+  count            = local.is_production ? 0 : 1
   name             = "${terraform.workspace}-app-monitor"
   domain           = "*.patient-deductions.nhs.uk"
   cw_log_enabled   = true
-  identity_pool_id = aws_cognito_identity_pool.rum_identity_pool.id
+  identity_pool_id = aws_cognito_identity_pool.rum_identity_pool[count.index].id
 
   app_monitor_configuration {
     allow_cookies       = true
@@ -16,6 +17,4 @@ resource "aws_cloudwatch_rum_app_monitor" "app_monitor" {
     session_sample_rate = 1.0
     telemetries         = ["errors", "performance", "http"]
   }
-  
-  count = local.is_production ? 0 : 1
 }
