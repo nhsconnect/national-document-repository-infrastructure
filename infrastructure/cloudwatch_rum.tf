d205c7ca9923b64484ca0062976de94530ea2de2
Ollie Beumkes <oliver.beumkes2@nhs.net>
2025-02-24 12:45:34 +0000

    Another refactor of policies and roles for rum

--- infrastructure/cloudwatch_rum.tf
+++ infrastructure/cloudwatch_rum.tf
@@ -1,39 +1,75 @@
-resource "aws_iam_role" "cognito_unauth_role" {
-  name = "${terraform.workspace}-cognito-unauth-role"
+locals {
+  is_mesh_enabled   = !contains(["ndra", "ndrb", "ndrc", "ndrd", "ndr-test"], terraform.workspace)
+  cognito_role_name = "${var.environment}-cognito-unauth-role"
+  rum_role_name     = "${var.environment}-rum-service-role"
+}
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "CognitoUnauthenticatedIdentity",
-        Effect = "Allow",
-        Principal = {
-          Service = "cognito-identity.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
+data "aws_iam_policy_document" "cognito_assume_role_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["cognito-identity.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "rum_assume_role_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["rum.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "cognito_access_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "mobileanalytics:PutEvents",
+      "cognito-sync:*",
+      "cognito-identity:*"
     ]
-  })
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "rum_management_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "rum:CreateAppMonitor",
+      "rum:DescribeAppMonitor",
+      "rum:DeleteAppMonitor",
+      "rum:UpdateAppMonitor",
+      "rum:TagResource",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "iam:PassRole"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "cognito_unauth_role" {
+  name               = local.cognito_role_name
+  assume_role_policy = data.aws_iam_policy_document.cognito_assume_role_policy.json
+}
+
+resource "aws_iam_role" "rum_service_role" {
+  name               = local.rum_role_name
+  assume_role_policy = data.aws_iam_policy_document.rum_assume_role_policy.json
 }
 
 resource "aws_iam_policy" "cognito_access_policy" {
-  name        = "${terraform.workspace}-cognito-access-policy"
+  name        = "${var.environment}-cognito-access-policy"
   description = "Policy for unauthenticated Cognito identities"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "mobileanalytics:PutEvents",
-          "cognito-sync:*",
-          "cognito-identity:*"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
+  policy      = data.aws_iam_policy_document.cognito_access_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "cognito_unauth_policy_attachment" {
@@ -41,46 +77,10 @@ resource "aws_iam_role_policy_attachment" "cognito_unauth_policy_attachment" {
   policy_arn = aws_iam_policy.cognito_access_policy.arn
 }
 
-resource "aws_iam_role" "rum_service_role" {
-  name = "${terraform.workspace}-rum-service-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "AllowRUMServiceToAssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "rum.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
 resource "aws_iam_policy" "rum_management_policy" {
-  name        = "${terraform.workspace}-rum-management-policy"
-  description = "Policy to allow RUM to create and manage app monitors"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "rum:CreateAppMonitor",
-          "rum:DescribeAppMonitor",
-          "rum:DeleteAppMonitor",
-          "rum:UpdateAppMonitor",
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
+  name        = "${var.environment}-rum-management-policy"
+  description = "Policy to manage RUM app monitors and associated logs"
+  policy      = data.aws_iam_policy_document.rum_management_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "rum_policy_attachment" {
@@ -88,14 +88,15 @@ resource "aws_iam_role_policy_attachment" "rum_policy_attachment" {
   policy_arn = aws_iam_policy.rum_management_policy.arn
 }
 
+
 resource "aws_cognito_identity_pool" "rum_identity_pool" {
-  count                            = local.is_production ? 0 : 1
-  identity_pool_name               = "${terraform.workspace}-rum-identity-pool"
+  count                            = local.is_mesh_enabled ? 0 : 1
+  identity_pool_name               = "${var.environment}-rum-identity-pool"
   allow_unauthenticated_identities = true
 }
 
 resource "aws_cognito_identity_pool_roles_attachment" "rum_identity_pool_roles" {
-  count            = local.is_production ? 0 : 1
+  count            = local.is_mesh_enabled ? 0 : 1
   identity_pool_id = aws_cognito_identity_pool.rum_identity_pool[0].id
 
   roles = {
@@ -105,7 +106,7 @@ resource "aws_cognito_identity_pool_roles_attachment" "rum_identity_pool_roles" 
 
 resource "aws_rum_app_monitor" "app_monitor" {
   count          = local.is_production ? 0 : 1
-  name           = "${terraform.workspace}-app-monitor"
+  name           = "${var.environment}-app-monitor"
   domain         = "*.patient-deductions.nhs.uk"
   cw_log_enabled = true
 
@@ -115,6 +116,7 @@ resource "aws_rum_app_monitor" "app_monitor" {
     session_sample_rate = 1.0
     telemetries         = ["errors", "performance", "http"]
   }
+
   tags = {
     ServiceRole = aws_iam_role.rum_service_role.arn
   }
