ae9dbd65bd346457efedb2da6c3a4666dcc78de6
Sam Whyte <sam.whyte@answerdigital.com>
2025-06-05 14:46:53 +0100

    temp reverting typo as var not being set

--- infrastructure/modules/cloudwatch/main.tf
+++ infrastructure/modules/cloudwatch/main.tf
@@ -11,6 +11,6 @@ resource "aws_cloudwatch_log_group" "ndr_cloudwatch_log_group" {
 }
 
 resource "aws_cloudwatch_log_stream" "log_stream" {
-  name           = "${terraform.workspace}_${var.cloudwatch_log_stream_name}_log_Stream"
+  name           = "${terraform.workspace}_${var.cloudwatch_log_steam_name}_log_Stream"
   log_group_name = aws_cloudwatch_log_group.ndr_cloudwatch_log_group.name
 }
