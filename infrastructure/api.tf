179530bccb070f1c6bd77e85fbcdc6a3e6006f4e
Sam Whyte <sam.whyte@answerdigital.com>
2025-06-06 12:34:12 +0100

    [NDR-102] remove null check

--- infrastructure/api.tf
+++ infrastructure/api.tf
@@ -93,23 +93,20 @@ resource "aws_api_gateway_stage" "ndr_api" {
   stage_name           = var.environment
   xray_tracing_enabled = var.enable_xray_tracing
 
-  dynamic "access_log_settings" {
-    for_each = module.cloudwatch.cloudwatch_log_group_arn != null ? [1] : []
-    content {
-      destination_arn = module.cloudwatch.cloudwatch_log_group_arn
-      format = jsonencode({
-        requestId      = "$context.requestId"
-        ip             = "$context.identity.sourceIp"
-        caller         = "$context.identity.caller"
-        user           = "$context.identity.user"
-        requestTime    = "$context.requestTime"
-        httpMethod     = "$context.httpMethod"
-        resourcePath   = "$context.resourcePath"
-        status         = "$context.status"
-        protocol       = "$context.protocol"
-        responseLength = "$context.responseLength"
-      })
-    }
+  access_log_settings {
+    destination_arn = module.cloudwatch.cloudwatch_log_group_arn
+    format = jsonencode({
+      requestId      = "$context.requestId"
+      ip             = "$context.identity.sourceIp"
+      caller         = "$context.identity.caller"
+      user           = "$context.identity.user"
+      requestTime    = "$context.requestTime"
+      httpMethod     = "$context.httpMethod"
+      resourcePath   = "$context.resourcePath"
+      status         = "$context.status"
+      protocol       = "$context.protocol"
+      responseLength = "$context.responseLength"
+    })
   }
 }
 
