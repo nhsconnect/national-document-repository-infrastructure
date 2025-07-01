5af5c86ebdd5ab0a293264c01f99615475a74437
Abbas Khan <abbas.khan10@nhs.net>
2025-07-01 16:51:28 +0100

    add TODO comments

--- scripts/cleanup_terraform_states.py
+++ scripts/cleanup_terraform_states.py
@@ -7,11 +7,12 @@ from botocore.exceptions import ClientError
 class CleanupTerraformStates:
     def __init__(self):
         self.env_folder = "env:/"
-        self.client = boto3.client("s3")
-        self.paginator = self.client.get_paginator('list_objects_v2')
+        self.s3_client = boto3.client("s3")
+        self.dynamo_client = boto3.client("dynamodb")
+        self.paginator = self.s3_client.get_paginator('list_objects_v2')
 
     def get_terraform_bucket(self) -> str:
-        response = self.client.list_buckets()
+        response = self.s3_client.list_buckets()
         buckets = response.get("Buckets")
         for bucket in buckets:
             if "ndr-dev-terraform-state" in bucket.get("Name"):
@@ -23,6 +24,7 @@ class CleanupTerraformStates:
         sys.exit(1)
 
     def remove_folder_objects(self, bucket_name:str, folder_prefix: str):
+        #TODO - Make sure we remove version history too
         print(f"Deleting all objects under: {folder_prefix}")
         pages = self.paginator.paginate(Bucket=bucket_name, Prefix=folder_prefix)
 
@@ -32,17 +34,24 @@ class CleanupTerraformStates:
                 objects_to_delete.append({'Key': obj['Key']})
 
         if objects_to_delete:
-            self.client.delete_objects(Bucket=bucket_name, Delete={'Objects': objects_to_delete})
+            #TODO - Check values before deleting
+            print(objects_to_delete)
+            #self.client.delete_objects(Bucket=bucket_name, Delete={'Objects': objects_to_delete})
 
-
+    #TODO - Feel free to remove if there's a better solution
     def empty_folder_check(self, bucket_name:str, folder_prefix: str):
         try:
-            self.client.head_object(Bucket=bucket_name, Key=folder_prefix)
-            self.client.delete_object(Bucket=bucket_name, Key=folder_prefix)
+            self.s3_client.head_object(Bucket=bucket_name, Key=folder_prefix)
+            self.s3_client.delete_object(Bucket=bucket_name, Key=folder_prefix)
             print(f"Deleted empty folder for: {folder_prefix}")
         except ClientError as e:
             if e.response['Error']['Code'] != "404":
                 print(f"No empty folder found to remove for: {folder_prefix}")
+
+    def delete_record_in_dynamo(self):
+        #TODO - query dynamo for matching tf state path based on your sandbox workspace
+        # Print out values first before deleting to confirm it is correct
+        pass
 
     def main(self, sandbox: str):
         tf_bucket = self.get_terraform_bucket()
@@ -55,8 +64,9 @@ class CleanupTerraformStates:
                 if parent_folder == sandbox:
                     print(parent_folder)
                     folder_prefix = f"{self.env_folder}{parent_folder}/"
-                    self.remove_folder_objects(bucket_name=tf_bucket, folder_prefix=folder_prefix)
-                    self.empty_folder_check(bucket_name=tf_bucket, folder_prefix=folder_prefix)
+                    self.delete_record_in_dynamo()
+                    #self.remove_folder_objects(bucket_name=tf_bucket, folder_prefix=folder_prefix)
+                    #self.empty_folder_check(bucket_name=tf_bucket, folder_prefix=folder_prefix)
 
 if __name__ == '__main__':
     sandbox = sys.argv[1]
