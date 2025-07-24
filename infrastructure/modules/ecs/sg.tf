640ad3effb5431825c702a35fb1438e89912822c
jameslinnell <jimlinnell@gmail.com>
2025-07-24 14:28:12 +0100

    Update Name tag on SG

--- infrastructure/modules/ecs/sg.tf
+++ infrastructure/modules/ecs/sg.tf
@@ -3,7 +3,7 @@ resource "aws_security_group" "ndr_ecs_sg" {
   description = "Allow TLS inbound traffic"
   vpc_id      = var.vpc_id
   tags = {
-    Name = "${terraform.workspace}-sg"
+    Name = var.sg_name
   }
 }
 
