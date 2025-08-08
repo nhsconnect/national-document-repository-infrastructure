a3d221423b074824aeb6e60e92c19207a9c2361a
jameslinnell <jimlinnell@gmail.com>
2025-08-08 12:21:48 +0100

    Remove imports

--- infrastructure/lambda-layers.tf
+++ infrastructure/lambda-layers.tf
@@ -21,23 +21,3 @@ module "lambda-layer-reports" {
   account_id = data.aws_caller_identity.current.account_id
   layer_name = "reports"
 }
-
-import {
-  to = module.lambda-layer-core.aws_lambda_layer_version.lambda_layer
-  id = "arn:aws:lambda:eu-west-2:${data.aws_caller_identity.current.account_id}:layer:${terraform.workspace}_core_lambda_layer:3"
-}
-
-import {
-  to = module.lambda-layer-data.aws_lambda_layer_version.lambda_layer
-  id = "arn:aws:lambda:eu-west-2:${data.aws_caller_identity.current.account_id}:layer:${terraform.workspace}_data_lambda_layer:3"
-}
-
-import {
-  to = module.lambda-layer-alerting.aws_lambda_layer_version.lambda_layer
-  id = "arn:aws:lambda:eu-west-2:${data.aws_caller_identity.current.account_id}:layer:${terraform.workspace}_alerting_lambda_layer:22"
-}
-
-import {
-  to = module.lambda-layer-reports.aws_lambda_layer_version.lambda_layer
-  id = "arn:aws:lambda:eu-west-2:${data.aws_caller_identity.current.account_id}:layer:${terraform.workspace}_reports_lambda_layer:1"
-}
