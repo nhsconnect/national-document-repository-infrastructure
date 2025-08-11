116f6fd720973598d63f953c8934408762f2e717
Sam Whyte <sam.whyte@answerdigital.com>
2025-08-11 16:10:02 +0100

    [NDR-108] temp readding table so that branch can destroy

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,10 +12,12 @@ terraform {
     }
   }
   backend "s3" {
-    use_lockfile = true
-    region       = "eu-west-2"
-    key          = "ndr/terraform.tfstate"
-    encrypt      = true
+    # TODO remove dynamodb_table
+    dynamodb_table = "ndr-terraform-locks"
+    use_lockfile   = true
+    region         = "eu-west-2"
+    key            = "ndr/terraform.tfstate"
+    encrypt        = true
   }
 }
 provider "aws" {
