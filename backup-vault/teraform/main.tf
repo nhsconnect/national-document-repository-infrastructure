4fb27adeed2fc298562469354a1d1320d2c7a56b
Sam Whyte <sam.whyte@answerdigital.com>
2025-08-13 09:42:59 +0100

    NDR-108 changing to new lock on backup-vault

--- backup-vault/teraform/main.tf
+++ backup-vault/teraform/main.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
   backend "s3" {
-    dynamodb_table = "ndr-backup-terraform-lock"
-    region         = "eu-west-2"
-    key            = "ndr/terraform.tfstate"
-    encrypt        = true
+    use_lockfile = true
+    region       = "eu-west-2"
+    key          = "ndr/terraform.tfstate"
+    encrypt      = true
   }
 }
 provider "aws" {
