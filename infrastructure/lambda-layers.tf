89e622e712f59f4c5ed3764715f8f4f1a54aab6a
jameslinnell <jimlinnell@gmail.com>
2025-08-13 11:01:10 +0100

    add aws_caller_identity to lambda_layers

--- infrastructure/lambda-layers.tf
+++ infrastructure/lambda-layers.tf
@@ -1,23 +1,19 @@
 module "lambda-layer-core" {
   source     = "./modules/lambda_layers"
-  account_id = data.aws_caller_identity.current.account_id
   layer_name = "core"
 }
 
 module "lambda-layer-data" {
   source     = "./modules/lambda_layers"
-  account_id = data.aws_caller_identity.current.account_id
   layer_name = "data"
 }
 
 module "lambda-layer-alerting" {
   source     = "./modules/lambda_layers"
-  account_id = data.aws_caller_identity.current.account_id
   layer_name = "alerting"
 }
 
 module "lambda-layer-reports" {
   source     = "./modules/lambda_layers"
-  account_id = data.aws_caller_identity.current.account_id
   layer_name = "reports"
 }
