44395a96bb41482ef7ce6f4c2a1b52f43b8a5d57
Sam Whyte <sam.whyte@answerdigital.com>
2025-08-14 09:18:27 +0100

    [NDR-108] re-adding everything

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -68,6 +68,21 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
 
 data "aws_caller_identity" "current" {}
 
+resource "aws_dynamodb_table" "dynamodb_terraform_state_lock" {
+  name           = "ndr-terraform-locks"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 variable "region" {
   type        = string
   default     = "eu-west-2"
