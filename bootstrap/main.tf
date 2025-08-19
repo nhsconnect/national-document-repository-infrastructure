1ea5fe620911a45c891456d7d6d773fb864427a9
Sam Whyte <sam.whyte@answerdigital.com>
2025-08-19 11:11:07 +0100

    [NDR-108] re-arrange

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -44,7 +44,6 @@ resource "aws_s3_bucket_versioning" "ndr_s3_state" {
   versioning_configuration {
     status = "Enabled"
   }
-
 }
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   bucket = aws_s3_bucket.ndr_lock_bucket.id
