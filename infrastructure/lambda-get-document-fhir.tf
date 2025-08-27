6665ca3763896f076b4d438aa0f22549016c167f
Megan <megan.bower4@nhs.net>
2025-08-27 16:01:38 +0100

    NDR-213 Move get document ref endpoint to mtls api

--- infrastructure/lambda-get-document-fhir.tf
+++ infrastructure/lambda-get-document-fhir.tf
@@ -1,13 +1,13 @@
 resource "aws_api_gateway_resource" "get_document_reference" {
   count       = 1
-  rest_api_id = aws_api_gateway_rest_api.ndr_doc_store_api.id
+  rest_api_id = aws_api_gateway_rest_api.ndr_doc_store_api_mtls.id
   parent_id   = module.fhir_document_reference_gateway[0].gateway_resource_id
   path_part   = "{id}"
 }
 
 resource "aws_api_gateway_method" "get_document_reference" {
   count            = 1
-  rest_api_id      = aws_api_gateway_rest_api.ndr_doc_store_api.id
+  rest_api_id      = aws_api_gateway_rest_api.ndr_doc_store_api_mtls.id
   resource_id      = aws_api_gateway_resource.get_document_reference[0].id
   http_method      = "GET"
   authorization    = "NONE"
@@ -30,10 +30,10 @@ module "get-doc-fhir-lambda" {
     module.ndr-lloyd-george-store.s3_read_policy_document,
   ]
   kms_deletion_window = var.kms_deletion_window
-  rest_api_id         = aws_api_gateway_rest_api.ndr_doc_store_api.id
+  rest_api_id         = aws_api_gateway_rest_api.ndr_doc_store_api_mtls.id
   resource_id         = aws_api_gateway_resource.get_document_reference[0].id
   http_methods        = ["GET"]
-  api_execution_arn   = aws_api_gateway_rest_api.ndr_doc_store_api.execution_arn
+  api_execution_arn   = aws_api_gateway_rest_api.ndr_doc_store_api_mtls.execution_arn
   lambda_environment_variables = {
     APPCONFIG_APPLICATION      = module.ndr-app-config.app_config_application_id
     APPCONFIG_ENVIRONMENT      = module.ndr-app-config.app_config_environment_id
