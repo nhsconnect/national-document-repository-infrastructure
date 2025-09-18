1e231f6e0e2dc04e7375604e65696be813b1f547
Megan <megan.bower4@nhs.net>
2025-09-18 10:26:42 +0100

    NDR-235 tf fmt

--- infrastructure/modules/ssm_parameter/main.tf
+++ infrastructure/modules/ssm_parameter/main.tf
@@ -3,13 +3,13 @@ resource "aws_ssm_parameter" "secret" {
   type        = var.type
   description = var.description
   value       = var.value
-  key_id = var.key_id
+  key_id      = var.key_id
   depends_on  = [var.resource_depends_on]
   tags = {
     Name = "${terraform.workspace}-ssm"
   }
 
-lifecycle {
+  lifecycle {
     ignore_changes = var.ignore_changes
   }
 }
