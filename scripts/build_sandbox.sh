3d021f80f9d518fdf8a64e7cc2aedd8d6bfef8b1
jameslinnell <jimlinnell@gmail.com>
2025-09-18 10:23:56 +0100

    Add check for aws environment

--- scripts/build_sandbox.sh
+++ scripts/build_sandbox.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 set -euo pipefail
 
+required_profile="NDR-Dev-RW"
+
+if [[ "${AWS_PROFILE:-}" != "$required_profile" ]]; then
+  echo "❌ Error: AWS_PROFILE must be set to \"$required_profile\""
+  echo "👉 Example: export AWS_PROFILE=$required_profile"
+  exit 1
+fi
+
 branch="${WORKSPACE:-$(git rev-parse --abbrev-ref HEAD)}"
 branch=$(echo "$branch" | sed 's/[^a-zA-Z0-9]//g')
 branch="${branch,,}"
 apply="${APPLY:-false}"
 
 # Forbidden branches
-forbidden_branches=("main" "prod" "pre-prod" "ndr-test" "ndr-dev")
+forbidden_workspaces=("main" "prod" "pre-prod" "ndr-test" "ndr-dev" "preprod" "ndrtest" "ndrdev")
 
-for fb in "${forbidden_branches[@]}"; do
+for fb in "${forbidden_workspaces[@]}"; do
   if [[ "$branch" == "$fb" ]]; then
-    echo "❌ Error: Deployment from branch '$branch' is not allowed."
+    echo "❌ Error: Deployment of workspace '$fb' is not allowed. If you are trying to deploy main to your sandbox then provide a workspace name."
     exit 1
   fi
 done
