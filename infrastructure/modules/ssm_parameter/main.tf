1e3afaa06ea359395a2acde64000b554c0ae0dea
Megan <megan.bower4@nhs.net>
2025-09-18 10:10:35 +0100

    NDR-235 add ssm params for certs and keys

--- infrastructure/modules/ssm_parameter/main.tf
+++ infrastructure/modules/ssm_parameter/main.tf
@@ -3,9 +3,14 @@ resource "aws_ssm_parameter" "secret" {
   type        = var.type
   description = var.description
   value       = var.value
+  key_id = var.key_id
   depends_on  = [var.resource_depends_on]
   tags = {
     Name = "${terraform.workspace}-ssm"
+  }
+
+lifecycle {
+    ignore_changes = var.ignore_changes
   }
 }
 
