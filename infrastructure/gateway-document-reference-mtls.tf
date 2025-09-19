44d62bbe9d0dd9484b1c580b793aa69ee492d202
jameslinnell <jimlinnell@gmail.com>
2025-09-19 10:13:01 +0100

    Remove api key

--- infrastructure/gateway-document-reference-mtls.tf
+++ infrastructure/gateway-document-reference-mtls.tf
@@ -4,7 +4,7 @@ module "fhir_document_reference_mtls_gateway" {
   parent_id           = aws_api_gateway_rest_api.ndr_doc_store_api_mtls.root_resource_id
   http_methods        = ["POST", "GET"]
   authorization       = "NONE"
-  api_key_required    = true
   gateway_path        = "DocumentReference"
   require_credentials = true
 }
+
