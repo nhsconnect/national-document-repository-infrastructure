f22bace0cec7ec77a0dca288c3366fc064d8396b
jameslinnell <jimlinnell@gmail.com>
2025-09-19 10:13:01 +0100

    Remove api key

--- infrastructure/lambda-get-document-reference-fhir.tf
+++ infrastructure/lambda-get-document-reference-fhir.tf
@@ -5,11 +5,10 @@ resource "aws_api_gateway_resource" "get_document_reference_mtls" {
 }
 
 resource "aws_api_gateway_method" "get_document_reference_mtls" {
-  rest_api_id      = aws_api_gateway_rest_api.ndr_doc_store_api_mtls.id
-  resource_id      = aws_api_gateway_resource.get_document_reference_mtls.id
-  http_method      = "GET"
-  authorization    = "NONE"
-  api_key_required = true
+  rest_api_id   = aws_api_gateway_rest_api.ndr_doc_store_api_mtls.id
+  resource_id   = aws_api_gateway_resource.get_document_reference_mtls.id
+  http_method   = "GET"
+  authorization = "NONE"
   request_parameters = {
     "method.request.path.id" = true
   }
